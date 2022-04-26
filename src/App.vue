a79d590d6ed28b891cb0bd19bceb9b68fff34afd
Gong Meng <18588264128@163.com>
2022-04-26 13:53:48 +0800

    jsonp以及跨域-接口代理

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import axios from 'axios'
+import jsonp from 'jsonp'
 
 export default {
   name: 'App',
@@ -16,13 +16,15 @@ export default {
   },
   data () {
     return {
-      age: 30
+      age: 30,
+      data: ''
     }
   },
   mounted () {
-    let url = "https://www.baidu.com/"
-    axios.get(url).then(() => {
-
+    let url = "/api/common/adver-getadver"
+    jsonp(url, (err, res) => {
+      let result = res
+      this.data = result
     })
   }
 }
