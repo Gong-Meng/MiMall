60b9e5ddbb2cd9e03f0c7a8ddb703b63b22f8eba
Gong Meng <18588264128@163.com>
2022-04-26 17:32:04 +0800

    接口错误拦截

--- src/main.js
+++ src/main.js
@@ -3,9 +3,25 @@ import App from './App.vue'
 import axios from 'axios'
 import router from './router'
 
-Vue.config.productionTip = false
-Vue.prototype.$axios = axios
+// 根据前端的跨域方式做调整
+axios.defaults.baseURL = '/api' // 此处采用接口代理
+axios.defaults.timeout = 8000
+/**
+ * 接口错误拦截
+ */
+axios.interceptors.response.use((response) => {
+  let res = response.data
+  if (res.status == 0) {
+    return res.data
+  } else if (res.status == 10) {
+    window.location.href = '/#/login'
+  } else {
+    alert(res.msg)
+  }
+})
 
+Vue.prototype.$axios = axios
+Vue.config.productionTip = false
 
 new Vue({
   router,
