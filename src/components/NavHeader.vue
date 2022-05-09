d05dfa715436c389d45fe77ba591eb688114da77
Gong Meng <18588264128@163.com>
2022-05-09 18:04:38 +0800

    nav-header组件四

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -23,15 +23,44 @@
                 <div class="header-menu">
                     <div class="item-menu">
                         <span>小米手机</span>
-                        <div class="chidren"></div>
+                        <div class="children">
+                            <ul>
+                                <li class="product">
+                                    <a href="" target="_blank">
+                                        <div class="pro-img">
+                                            <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/88ae9b85c5f8fbdae2ea98d58a045e1e.png?thumb=1&w=160&h=110&f=webp&q=90" alt="">            
+                                        </div>
+                                        <div class="pro-name">
+                                            小米CC9
+                                        </div>
+                                        <div class="pro-price">
+                                            1799元
+                                        </div>
+                                    </a>
+                                </li>
+                                <li class="product">
+                                    <a href="" target="_blank">
+                                        <div class="pro-img">
+                                            <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/88ae9b85c5f8fbdae2ea98d58a045e1e.png?thumb=1&w=160&h=110&f=webp&q=90" alt="">            
+                                        </div>
+                                        <div class="pro-name">
+                                            小米CC9
+                                        </div>
+                                        <div class="pro-price">
+                                            1799元
+                                        </div>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                     <div class="item-menu">
                         <span>RedMi手机</span>
-                        <div class="chidren"></div>
+                        <div class="children"></div>
                     </div>
                     <div class="item-menu">
                         <span>电视</span>
-                        <div class="chidren"></div>
+                        <div class="children"></div>
                     </div>
                 </div>
                 <div class="header-search">
@@ -54,6 +83,7 @@ export default {
 <style lang="scss">
     @import './../assets/scss/base.scss';
     @import './../assets/scss/mixin.scss';
+    @import './../assets/scss/config.scss';
     .header{
         .nav-topbar{
             height: 39px;
@@ -81,6 +111,7 @@ export default {
         }
         .nav-header{
             .container{
+                position: relative;
                 height: 112px;
                 @include flex();
                 .header-logo{
@@ -122,7 +153,61 @@ export default {
                             cursor: pointer;
                         }
                         &:hover{
-                            color: red;
+                            color: $colorA;
+                            .children{
+                                height: 220px;
+                            }
+                        }
+                        .children{
+                            position: absolute;
+                            top: 112px;
+                            left: 0;
+                            width: 1226px;
+                            height: 220px;
+                            border-top: 1px solid #e5e5e5;
+                            box-shadow: 0px 7px 6px 0px rgba(0,0,0,0.11);
+                            z-index: 10;
+                           .product{
+                                position: relative;
+                                float: left;
+                                width: 16.6%;
+                                height: 220px;
+                                font-size: 12px;
+                                line-height: 12px;
+                                text-align: center;
+                                a{
+                                    display: inline-block;
+                                }
+                                img{
+                                    height: 111px;
+                                    width: auto;
+                                    margin-top: 26px;
+                                }
+                                .pro-img{
+                                    height: 137px;
+                                }
+                                .pro-name{
+                                    font-weight: bold;
+                                    margin-top: 19px;
+                                    margin-bottom: 8px;
+                                    color: $colorB;
+                                }
+                                .pro-price{
+                                    color: $colorA;
+                                }
+                                &:before{
+                                    content: '';
+                                    position: absolute;
+                                    top: 28px;
+                                    right: 0;
+                                    border-left:1px solid $colorF;
+                                    height: 100px;
+                                    width: 1px;
+                                }
+                                &:last-child::before{
+                                    display: none;
+                                }
+                            }
                         }
                     }
                 }
