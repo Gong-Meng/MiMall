022ed0d19a87172a6f1aeeb9e80afe9b3cdb87bb
Gong Meng <18588264128@163.com>
2022-05-16 17:47:11 +0800

    5-10首页轮播菜单实现2

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -7,50 +7,36 @@
                         <li class="menu-item">
                             <a href="javascript:;">手机 电话卡</a>
                             <div class="children">
-
+                                <ul v-for="(item, i) in menuList" :key="i">
+                                    <li v-for="(sub, j) in item" :key="j">
+                                        <a :href="sub?'/#/product/'+sub.id:''">
+                                            <img :src="sub?sub.img:'/imgs/item-box-1.png'" alt="">
+                                            {{sub?sub.name:'小米9'}}
+                                        </a> 
+                                    </li>
+                                </ul>
                             </div>
                         </li>
                         <li class="menu-item">
                             <a href="javascript:;">电视 盒子</a>
-                            <div class="children">
-
-                            </div>
                         </li>
                         <li class="menu-item">
                             <a href="javascript:;">笔记本 平板</a>
-                            <div class="children">
-
-                            </div>
                         </li>
                         <li class="menu-item">
                             <a href="javascript:;">家电 插线板</a>
-                            <div class="children">
-
-                            </div>
                         </li>
                         <li class="menu-item">
                             <a href="javascript:;">出行 穿戴</a>
-                            <div class="children">
-
-                            </div>
                         </li>
                         <li class="menu-item">
                             <a href="javascript:;">智能 路由器</a>
-                            <div class="children">
-
-                            </div>
                         </li>
                         <li class="menu-item">
                             <a href="javascript:;">电源 配件</a>
-                            <div class="children">
-
-                            </div>
                         </li>
                         <li class="menu-item">
                             <a href="javascript:;">生活 箱包</a>
-                            <div class="children">
-
-                            </div>
                         </li>
                     </ul>
                 </div>
@@ -133,6 +119,35 @@ export default {
                     id: '',
                     img: '/imgs/slider/slide-5.jpg'
                 }
+            ],
+            menuList: [
+                [
+                    {
+                        id: 30,
+                        img: '/imgs/item-box-1.png',
+                        name: '小米cc9',
+                    },
+                    {
+                        id: 31,
+                        img: '/imgs/item-box-2.png',
+                        name: '小米8青春版',
+                    },
+                    {
+                        id: 32,
+                        img: '/imgs/item-box-3.jpg',
+                        name: 'Redmi K20 Pro',
+                    },
+                    {
+                        id: 33,
+                        img: '/imgs/item-box-4.jpg',
+                        name: '移动4G专区',
+                    }
+                ],
+                [0,0,0,0],
+                [0,0,0,0],
+                [0,0,0,0],
+                [0,0,0,0],
+                [0,0,0,0]
             ]
         }
     }
@@ -172,6 +187,41 @@ export default {
                         }
                         &:hover{
                             background-color: $colorA;
+                            .children{
+                                display: block;
+                            }
+                        }
+                        .children{
+                            display: none;
+                            width: 962px;
+                            height: 451px;
+                            background-color: $colorG;
+                            position: absolute;
+                            top: 0;
+                            left: 264px;
+                            border: 1px solid $colorH;
+                            ul{
+                                display: flex;
+                                justify-content: space-between;
+                                height: 75px;
+                                li{
+                                    height: 75px;
+                                    line-height: 75px;
+                                    width: 241px;
+                                    flex: 1;
+                                    padding-left: 23px;
+                                }
+                                a{
+                                    color: $colorB;
+                                    font-size: 14px;
+                                }
+                                img{
+                                    width: 42px;
+                                    height: 35px;
+                                    vertical-align: middle;
+                                    margin-right: 15px;
+                                }
+                            }
                         }
                     }
                 }
