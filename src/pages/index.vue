83e726a70e7f981f0414124f1f10a1307c6ff92c
Gong Meng <18588264128@163.com>
2022-05-16 11:59:34 +0800

    5-9首页轮播菜单实现1

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,6 +2,58 @@
     <div class="index">
         <div class="container">
             <div class="swiper-box">
+                <div class="nav-menu">
+                    <ul class="menu-wrap">
+                        <li class="menu-item">
+                            <a href="javascript:;">手机 电话卡</a>
+                            <div class="children">
+
+                            </div>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">电视 盒子</a>
+                            <div class="children">
+
+                            </div>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">笔记本 平板</a>
+                            <div class="children">
+
+                            </div>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">家电 插线板</a>
+                            <div class="children">
+
+                            </div>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">出行 穿戴</a>
+                            <div class="children">
+
+                            </div>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">智能 路由器</a>
+                            <div class="children">
+
+                            </div>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">电源 配件</a>
+                            <div class="children">
+
+                            </div>
+                        </li>
+                        <li class="menu-item">
+                            <a href="javascript:;">生活 箱包</a>
+                            <div class="children">
+
+                            </div>
+                        </li>
+                    </ul>
+                </div>
                 <swiper :options="swiperOption">
                     <swiper-slide v-for="(item, index) in slideList" :key="index">
                         <a v-bind:href="'/#/product'+item.id">
@@ -88,14 +140,51 @@ export default {
 </script>
 
 <style lang="scss">
+    @import './../assets/scss/config.scss';
+    @import './../assets/scss/mixin.scss';
     .index{
         .swiper-box{
+            .nav-menu{
+                position: absolute;
+                width: 264px;
+                height: 451px;
+                z-index: 9;
+                padding: 26px 0;
+                background-color: #55585a7a;
+                box-sizing: border-box;
+                .menu-wrap{
+                    .menu-item{
+                        height: 50px;
+                        line-height: 50px;
+                        a{
+                            display: block;
+                            position: relative;
+                            font-size: 16px;
+                            color: #fff;
+                            padding-left: 30px;
+                            &:after{
+                                position: absolute;
+                                right: 30px;
+                                top: 17.5px;
+                                content: '';
+                                @include bgImg(10px, 15px, '/imgs/icon-arrow.png')
+                            }
+                        }
+                        &:hover{
+                            background-color: $colorA;
+                        }
+                    }
+                }
+            }
             .swiper-container{
                 height: 451px;
-            }
-            img{
-                width: 100%;
-                height: 100%;
+                .swiper-button-prev{
+                   left: 274px; 
+                }
+                img{
+                    width: 100%;
+                    height: 100%;
+                }
             }
         }
     }
