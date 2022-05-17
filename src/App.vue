8b38b84129a0516e900ed79e1ee2f70504cf4410
Gong Meng <18588264128@163.com>
2022-05-17 11:13:57 +0800

    5-14Modal组件实现1

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,6 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
 @import './assets/scss/reset.scss';
 </style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -89,13 +89,25 @@
                 </div>
             </div>
         </div>
-       <service-bar></service-bar>
+        <service-bar></service-bar>
+        <modal 
+            title="提示" 
+            sureText="查看购物车" 
+            btnType="1" 
+            modalType="middle"
+            v-bind:showModal="true"
+        >
+            <template v-slot:body>
+                <p>商品添加成功</p>
+            </template>
+        </modal>
     </div>
 </template>
 
 
 <script>
 import ServiceBar from './../components/ServiceBar'
+import Modal from './../components/Modal'
 import {Swiper, SwiperSlide} from 'vue-awesome-swiper'
 import 'swiper/css/swiper.css'
 export default {
@@ -103,7 +115,8 @@ export default {
     components: {
         Swiper,
         SwiperSlide,
-        ServiceBar
+        ServiceBar,
+        Modal
     },
     data () {
         return {
@@ -207,9 +220,10 @@ export default {
             this.$axios.get('/products', {
                 params: {
                     categoryId: 100012,
-                    pageSize:8
+                    pageSize:14
                 }
             }).then((res) => {
+                res.list = res.list.slice(6, 14);
                 this.phoneList = [res.list.slice(0, 4),res.list.slice(4, 8)]
             })
         }
