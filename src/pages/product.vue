2392294779c2b41970d5e8fb2196e408a4c738d3
Gong Meng <18588264128@163.com>
2022-05-18 18:30:22 +0800

    8-5 slide动画实现2

--- src/pages/product.vue
+++ src/pages/product.vue
@@ -43,11 +43,11 @@
             <div class="item-video">
                 <h2>60帧超慢动作摄影<br/>慢慢回味每一瞬间的精彩</h2>
                 <p>后置960帧电影般超慢动作视频，将眨眼间的美妙展现得淋漓尽致！<br/>更能AI 精准分析视频内容，15个场景智能匹配背景音效。</p>
-                <div class="video-bg" @click="showSlide=true"></div>
+                <div class="video-bg" @click="showSlide='slideDown'"></div>
                 <div class="video-box">
-                    <div class="overlay" v-if="showSlide"></div>
-                    <div class="video" v-bind:class="{'slide':showSlide}">
-                        <span class="icon-close" @click="showSlide=false"></span>
+                    <div class="overlay" v-if="showSlide=='slideDown'"></div>
+                    <div class="video" v-bind:class="showSlide">
+                        <span class="icon-close" @click="showSlide='slideUp'"></span>
                         <video src="/imgs/product/video.mp4" muted autoplay controls></video>
                     </div>
                 </div>
@@ -69,7 +69,8 @@ export default {
     },
     data () {
         return {
-            showSlide: false,
+            // showSlide: false,  transition 处理
+            showSlide: '',
             swiperOption:{
                 autoplay:true,
                 slidesPerView:3,
@@ -176,6 +177,26 @@ export default {
                     opacity: .4;
                     z-index: 10;
                 }
+                @keyframes slideDown {
+                    from{
+                        top: -50%;
+                        opacity: 0;
+                    }
+                    to{
+                        top: 50%;
+                        opacity: 1;
+                    }
+                }
+                @keyframes slideUp {
+                    from{
+                        top: 50%;
+                        opacity: 1;
+                    }
+                    to{
+                        top: -50%;
+                        opacity: 0;
+                    }
+                }
                 .video{
                     position: fixed;
                     top: -50%;
@@ -185,11 +206,20 @@ export default {
                     width: 1000px;
                     height: 536px;
                     opacity: 0;
-                    transition: all .6s;
-                    &.slide{
+                    &.slideDown{
+                        animation: slideDown .6s linear;
                         top: 50%;
                         opacity: 1;
                     }
+                    &.slideUp{
+                        animation: slideUp .6s linear;
+                    }
+                    // transition 过渡处理
+                    // transition: all .6s;
+                    // &.slide{
+                    //     top: 50%;
+                    //     opacity: 1;
+                    // }
                     .icon-close{
                         position: absolute;
                         top: 20px;
