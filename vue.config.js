149003042edbaec57b2a55c711aac3ecdfb77d19
Gong Meng <18588264128@163.com>
2022-06-09 15:41:28 +0800

    上线处理

--- vue.config.js
+++ vue.config.js
@@ -16,7 +16,7 @@ module.exports = {
     outputDir: 'dist', // 打包输出目录
     // indexPath: 'index2.html', // 根页面名称
     lintOnSave: true, // eslint语法校验开关
-    // productionSourceMap: false, // sourceMap调试开关
+    productionSourceMap: false, // sourceMap调试开关
     chainWebpack: (config) => {
         config.plugins.delete('prefetch')
     }
